16b1934f4b08dfd454b0b62c5ef77cafbde8f282
Emin Eker <emineker@bil.omu.edu.tr>
2011-08-04 19:37:17 +0300

    .

--- admin/inc/review.php
+++ admin/inc/review.php
@@ -6,12 +6,15 @@ include 'init.php';
 $TABLE = F3::get('SESSION.TABLE');
 
 F3::get('DB')->schema($TABLE, 0);// 0 nolu kayıt gibi Field alanlarını al.
+F3::set('SESSION.DB', F3::get('DB'));// 0 nolu kayıt gibi Field alanlarını al.
 
 $fields = array();
-foreach (F3::get('DB->result') as $gnl => $blg)
+foreach (F3::get('DB->result') as $gnl => $blg){
+	echo $blg['Field'];
 	array_push($fields, $blg['Field']);
+}
 
-F3::set('fields', $fields);
+F3::set('SESSION.fields', $fields);
 
 $table = new Axon($TABLE);
 $table->afind();
