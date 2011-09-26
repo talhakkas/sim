9a2433f1dbf30b177423049c3a3e0d95beac56e3
emin eker <emineker@bil.omu.edu.tr>
2011-09-26 20:07:55 +0300

    .

--- student/index.php
+++ student/index.php
@@ -77,6 +77,42 @@ function cases() {
 }
 
 
+F3::route('GET /ilac', function() {render('ilac', 'ilaç ekranı');});
+F3::route('GET /test', function() {
+        if (F3::get("SESSION.special") != 1)
+                return F3::call('login');
+
+        $dis = DB::sql('select name from discipline');
+        $discipline = array();
+        foreach ($dis as $key => $value)
+                $discipline[$key+1] = $value['name'];
+
+        F3::set('discipline', $discipline);
+
+        //$olgu = 2;
+        $olgu = F3::get('SESSION.olgu');
+
+        F3::set('event', $olgu);
+        $event = new Axon('event');
+        $event->load("event_id='$olgu'");
+
+        $story = new Axon('story');
+        $story->load("story_id='$event->story_id'");
+        F3::set('story', $story);
+
+        $patient = new Axon('patient');
+        $patient->load("patient_id='$event->patient_id'");
+        F3::set('patient', $patient);
+
+        $event->close;
+        $story->close;
+        $patient->close;
+
+        render('test', 'yeni olgu ekranı');
+}
+);
+
+
 F3::route("GET /*"      , 'login');
 F3::route("GET /printly*"      , 'printly');
 F3::route("POST /login", 'login.php');
