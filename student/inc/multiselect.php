6aa274d1fc16a0be9ba927f9fecf87bb2d517660
emin eker <emineker@bil.omu.edu.tr>
2011-09-28 16:16:03 +0300

    .

--- student/inc/multiselect.php
+++ student/inc/multiselect.php
@@ -4,6 +4,7 @@ function multi(){
         $discipline = DB::sql('select name from discipline');
         $survey = DB::sql('select * from d_survey');
         $parent = DB::sql('select * from parent');
+        //print_r($survey);
 
         $a = '';
         $a .= '<ul>';
@@ -14,17 +15,18 @@ function multi(){
         foreach ($discipline as $key => $val){
                 $a .= '<div id="frag-'.$key.'">';
                 foreach ($parent as $k => $v){
-                        if ($v['parent_id'][0] == $key+1){
+                        $parent_id = substr($v['parent_id'], 0, 2);
+                        if ($parent_id == $key+1){
                                 $a .= '<p class="answer">' . $v['name'] . '</p>';
                                 $a .= '<select multiple="multiple" size="5" style="width:885px;">';
-                                $a .= '        <option value="1">Değer1</option>';
-                                $a .= '        <option value="2">Değer2</option>';
-                                $a .= '        <option value="3">Değer3</option>';
-                                #foreach ($survey as $x => $y){
-                                #        if ($y['d_survey_id'][0] == $key+1){
-                                #                echo '<option value="">Değer</option>';
-                                #        }
-                                #}
+                                foreach ($survey as $x => $y){
+                                        $survey_id = substr($y['d_survey_id'], 2, 2);
+                                        $_id = substr($y['d_survey_id'], 4, 2);
+                                        if ($survey_id == $parent_id){
+                                                //echo $x;
+                                                $a .= '<option value="'. $_id .'">Değer</option>';
+                                        }
+                                }
                                 $a .= '</select>';
                         }
                 }
