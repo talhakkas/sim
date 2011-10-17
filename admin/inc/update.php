d3abf2dacc3a8c12b4061f15d526f559bd292a60
gdemir <gdemir@bil.omu.edu.tr>
2011-10-17 10:20:22 +0300

    . güncelleme kontrol

--- admin/inc/update.php
+++ admin/inc/update.php
@@ -6,16 +6,22 @@ if (!F3::exists('error')) {
 	$TABLE = F3::get('SESSION.TABLE');
 	$KEY = F3::get('SESSION.KEY');
 	$key = F3::get('SESSION.key');
+	$request_key = F3::get('REQUEST.'.$KEY);
 
 	$table = new Axon($TABLE);
 
 	// oturumdan veriyi yükleyelim
 	$table->load("$KEY='$key'");
+	if ($key != $request_key && $table->found("$KEY='$request_key'")) {
+		F3::set('error', "$request_key id'li kayıt var, güncelleme gerçekleşmedi");
+		return F3::call('show');
+	}
 
 	foreach ($_POST as $gnl => $blg)
 		if ($gnl != 'photo') // photo'yu kaydetme
 			$table->$gnl = $blg;
 
+	F3::set('SESSION.key', $table->$KEY);
 	$table->save();
 
 	$table = new Axon($TABLE);
