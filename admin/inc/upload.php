ce695f5813bfa9b19fa7f3231a61a7634adaaf0d
gdemir <gdemir@bil.omu.edu.tr>
2011-10-17 10:29:12 +0300

    csv yükleme ismi:$TABLE

--- admin/inc/upload.php
+++ admin/inc/upload.php
@@ -6,10 +6,9 @@ $KEY = F3::get('SESSION.KEY');
 $csv_key = F3::get('REQUEST.csv_key');
 $csv_file = F3::get('FILES.csv_file');
 
-$csv_file_name = date("Y-m-d_h:i:s");
 $up = new Upload(array('csv'));
 
-if ($response = ($up->load($TABLE, $csv_file_name, $csv_file, true)))
+if ($response = ($up->load($TABLE, $TABLE, $csv_file, true)))
 	if (! $response[0])
 		F3::set('error', $response[1]);
 
@@ -19,7 +18,7 @@ if (F3::exists('error')) // yükleme sırasında hata var mı?
 F3::get('DB')->schema($TABLE, 0);// 0 nolu kayıt gibi Field alanlarını al.
 $fields = F3::get('DB->result'); // gerçek alanlar
 
-$csv_path = '../public/upload/' . $TABLE . '/' . $csv_file_name . '.csv';
+$csv_path = '../public/upload/' . $TABLE . '/' . $TABLE . '.csv';
 
 $_rows = Csv::read($csv_path, $csv_key);
 $_fields = array_shift($_rows);
@@ -35,7 +34,6 @@ foreach ($_rows as $row) {
 		$table->$field = array_shift($row);
 	$table->save();
 }
-
 
 F3::reroute('/review');
 
