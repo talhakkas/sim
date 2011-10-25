d9723637e0f79f80ac002c21c484445cec498485
emineker <emineker@bil.omu.edu.tr>
2011-10-25 13:52:40 +0300

    .

--- yolgu/inc/cadd.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$cid = maxID("cid", "ncase") + 1;
-	F3::set('SESSION.cid', $cid);
-
-	$all_nodes = nodeList($cid);
-	F3::set('SESSION.all_nodes', $all_nodes);
-	//cilkle();
-	render('case', 'Yeni Ekle');
-?>
