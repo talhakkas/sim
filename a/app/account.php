4714afc2948234c18c78dab7b1aa67f4c28eeae2
emineker <emineker@bil.omu.edu.tr>
2011-11-24 12:12:15 +0200

    hmm

--- a/app/account.php
+++ a/app/account.php
@@ -72,7 +72,7 @@ class Account extends F3instance {
 		$user->load("email='$email'");
 
 		if (!$user->dry()) {
-			$to = $user->email;
+			$to = (string)$user->email;
 			$from = "noreply@sim.omu.edu.tr";
 			$subject = "sim parolası";
 			$message = "parolanız: " . $email->password;
