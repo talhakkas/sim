eb5007b6abeb7c1e56db1e26336e589969782d6d
emineker <emineker@bil.omu.edu.tr>
2011-11-25 22:05:59 +0200

    .

--- a/inc/lib.php
+++ a/inc/lib.php
@@ -14,6 +14,7 @@ F3::config("../.f3.ini");
 F3::set('DB', new DB('mysql:host=localhost;port=3306;dbname=' . F3::get('dbname'), F3::get('dbuser'), F3::get('dbpass')));
 F3::set('SR', '/' . strtok($_SERVER["SCRIPT_NAME"], '/'));
 
+
 $locale = F3::get('SESSION.lang')? F3::get('SESSION.lang') : F3::get('lang');
 
 T_setlocale(LC_MESSAGES, $locale);
