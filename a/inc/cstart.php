effc1fce28b2c0d2d27d45b58cff9078e68abedc
emineker <emineker@bil.omu.edu.tr>
2011-11-29 20:17:13 +0200

    .

--- a/inc/cstart.php
+++ a/inc/cstart.php
@@ -1,11 +1,14 @@
 <?php
+
+echo 'clist';
+/*
 	$cid = F3::get('PARAMS.cid') ? F3::get('PARAMS.cid'):1;
 	F3::set('SESSION.cid', $cid);
 
 	$table = new Axon("ncase");
 	$datas = $table->afind("cid='$cid'");
 	$cdata = $datas[0];
-
+print_r($cdata);
 	$id = $cdata['bdugumu'];
 	F3::set('SESSION.id', $id);
 
@@ -13,5 +16,7 @@
 	F3::set('SESSION.skey', mt_rand());
 	F3::set('SESSION.stime', microtime(true));
 
-	F3::reroute("/show/$cid/$id/1");	
+	//F3::reroute("/show/$cid/$id/1");
+*/
+
 ?>
