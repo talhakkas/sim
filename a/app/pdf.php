33e21a7b0937f2eedf7eda30add7379e4c00b56e
emineker <emineker@bil.omu.edu.tr>
2011-12-02 21:23:51 +0200

    .

--- a/app/pdf.php
+++ a/app/pdf.php
@@ -7,10 +7,9 @@ class Pdf extends F3instance {
 		$pdf->SetTitle('Web Tabanlı Medikal Simülasyon Eğitim Merkezi');
 		$pdf->SetAuthor('sim');
 
-		$pdf->SetFont('freeserif', '', 12);
+		$pdf->SetFont('freeserif','', 12);
 		$pdf->SetMargins(20, 20, 20);
 		$pdf->setPrintHeader(false);
-		$pdf->setPrintFooter(false);
 		$pdf->AddPage();
 
 		$pdf->SetFont('freeserif', 'B', 12);
@@ -32,39 +31,50 @@ class Pdf extends F3instance {
 		);
 
 		// basit öğrenci bilgisi
-		$pdf->SetFont('freeserif', 'B', 9);
-		$pdf->Cell(0, 5, "Öğrenci Bilgileri", 0, 1, 'L');
-
+		$pdf->SetFont('freeserif', 'B',10);
+		$pdf->setCellPaddings(14, 1, 3, 1);
+		$pdf->Cell(170, 7, "Öğrenci Bilgileri", 1, 1, 'L');
 		$pdf->SetFont('freeserif', '', 8);
+		$pdf->SetFillColor(222,222,239);
+		$pdf->setCellPaddings(9, 1, 3, 1);
 		foreach ($bilgi as $key => $value) {
-			$pdf->MultiCell(30, 1, "$value:", 0, 'L', 0, 0, '35', '', true);
-			$pdf->MultiCell(80, 1, $userinfo[$key], 0, 'L', 0, 0, '',   '', true);
-			$pdf->Ln(5);
+			$pdf->MultiCell(85, 1, "$value:", 1, 'L', 1, 0, '', '', true);
+			$pdf->MultiCell(85, 1, $userinfo[$key], 1, 'L', 0, 0, '',   '', true);
+			$pdf->Ln();
 		}
-		$pdf->Ln(5);
-
+		$pdf->Ln(9);
 		// olgu raporu
-		$pdf->SetFont('freeserif', 'B', 9);
-		$pdf->Cell(0, 5, "Olgu Raporu", 0, 1, 'L');
+
+		$pdf->SetFont('freeserif', 'B', 10);
+		$pdf->setCellPaddings(14, 1, 3, 1);
+		$pdf->Cell(170, 7, "Olgu Raporu", 1, 1, 'L');
+
+		$pdf->SetFillColor(145,197,239);
+		$pdf->SetFont('freeserif', '', 9);
+		$pdf->setCellPaddings(5, 1, 3, 1);
+		$pdf->MultiCell(85, 1, "Olgu Adı:", 1, 'L', 1, 0, '', '', true);
+		$pdf->MultiCell(85, 1, F3::get('SESSION.case_title'), 1, 'L', 1, 0, '',   '', true);
+		$pdf->Ln();
 
 		$pdf->SetFont('freeserif', '', 8);
-		$pdf->MultiCell(30, 1, "Olgu Adı:", 0, 'L', 0, 0, '35', '', true);
-		$pdf->MultiCell(80, 1, F3::get('SESSION.case_title'),       0, 'L', 0, 0, '',   '', true);
-		$pdf->Ln(5);
+		$pdf->setCellPaddings(11, 1, 3, 1);
+			$i = 0;
+			foreach ($tdata as $id => $dict) {
+					$pdf->SetFillColor(220,240, 255);
+					if($i == 0){
+						$pdf->SetFillColor(240, 255, 255);
+					}
+					$pdf->MultiCell(85, 1, $dict['title'], 1, 'L', 1, 0, '', '', true);
+					$pdf->MultiCell(85, 1, $dict['puan'], 1, 'L', 1, 0, '',   '', true);
+					$pdf->Ln();
+					$i = !$i;
+			}
 
-		$pdf->SetFont('freeserif', '', 8);
-		foreach ($tdata as $id => $dict) {
-			$pdf->MultiCell(30, 1, $dict['title'], 0, 'L', 0, 0, '35', '', true);
-			$pdf->MultiCell(80, 1, $dict['puan'], 0, 'L', 0, 0, '',   '', true);
-			$pdf->Ln(5);
-		}
 		$pdf->Ln(5);
-
 		$pdf->Output( $userinfo['id'], 'D');
 	}
-
-
 function student_report() {
+
 		$pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
 		$pdf->SetTitle('Web Tabanlı Medikal Simülasyon Eğitim Merkezi');
 		$pdf->SetAuthor('sim');
@@ -93,27 +103,29 @@ function student_report() {
 		);
 
 		// basit öğrenci bilgisi
-		$pdf->SetFont('freeserif', 'B', 9);
-		$pdf->Cell(160, 7, "Öğrenci Bilgileri", 1, 1, 'L');
-		$pdf->Ln();
+		$pdf->SetFont('freeserif', 'B',10);
+		$pdf->setCellPaddings(14, 1, 3, 1);
+		$pdf->Cell(170, 7, "Öğrenci Bilgileri", 1, 1, 'L');
 		$pdf->SetFont('freeserif', '', 8);
+		$pdf->SetFillColor(222,222,239);
+		$pdf->setCellPaddings(9, 1, 3, 1);
 		foreach ($bilgi as $key => $value) {
-			$pdf->MultiCell(50, 1, "$value:", 0, 'L', 0, 0, '35', '', true);
-			$pdf->MultiCell(80, 1, $userinfo[$key], 0, 'L', 0, 0, '',   '', true);
-			$pdf->Ln(5);
+			$pdf->MultiCell(85, 1, "$value:", 1, 'L', 1, 0, '', '', true);
+			$pdf->MultiCell(85, 1, $userinfo[$key], 1, 'L', 0, 0, '',   '', true);
+			$pdf->Ln();
 		}
-		$pdf->Ln(5);
-
+		$pdf->Ln(9);
 		// olgu raporu
 
 		$pdf->SetFont('freeserif', 'B', 10);
-		$pdf->Cell(160, 5, "Olgu Raporu", 1, 1, 'L');
+		$pdf->setCellPaddings(14, 1, 3, 1);
+		$pdf->Cell(170, 7, "Olgu Raporu", 1, 1, 'L');
 
 		$pdf->SetFillColor(145,197,239);
 		$pdf->SetFont('freeserif', '', 9);
 		$pdf->setCellPaddings(5, 1, 3, 1);
-		$pdf->MultiCell(80, 1, "Olgu Adı:", 1, 'L', 1, 0, '', '', true);
-		$pdf->MultiCell(80, 1, F3::get('SESSION.case_title'), 1, 'L', 1, 0, '',   '', true);
+		$pdf->MultiCell(85, 1, "Olgu Adı:", 1, 'L', 1, 0, '', '', true);
+		$pdf->MultiCell(85, 1, F3::get('SESSION.case_title'), 1, 'L', 1, 0, '',   '', true);
 		$pdf->Ln();
 
 		$pdf->SetFont('freeserif', '', 8);
@@ -124,17 +136,15 @@ function student_report() {
 					if($i == 0){
 						$pdf->SetFillColor(240, 255, 255);
 					}
-					$pdf->MultiCell(80, 1, $dict['title'], 1, 'L', 1, 0, '', '', true);
-					$pdf->MultiCell(80, 1, $dict['puan'], 1, 'L', 1, 0, '',   '', true);
+					$pdf->MultiCell(85, 1, $dict['title'], 1, 'L', 1, 0, '', '', true);
+					$pdf->MultiCell(85, 1, $dict['puan'], 1, 'L', 1, 0, '',   '', true);
 					$pdf->Ln();
 					$i = !$i;
 			}
 
 		$pdf->Ln(5);
-
 		$pdf->Output( $userinfo['id'], 'D');
 	}
-
 
 	function beforeroute() {
 		require("/opt/tcpdf/tcpdf.php");
